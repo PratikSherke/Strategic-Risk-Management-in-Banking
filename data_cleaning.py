97f56adca9f7bf51a6ceebc1deef206f71377fed
@Pratik7 <pratiksherke7@gmail.com>
2025-08-02 20:18:59 +0530

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,7 +3,7 @@ import numpy as np
 from datetime import datetime
 
 # Load dataset
-df = pd.read_excel('Raw data.xlsx', sheet_name='Clients - Banking')
+df = pd.read_csv('Raw data.csv', sheet_name='Clients - Banking')
 
 # Data Cleaning
 # Handle missing values
